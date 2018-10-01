497048623a17f40e6cd0351f3a7bf49bc11ca7f5
shruthi T P <shruthtp@in.ibm.com>
2018-10-01 17:21:10 +0530

    ADD remove script

--- server/server.js
+++ server/server.js
@@ -53,6 +53,27 @@ app.get('/todos/:id', (req, res) => {
     }).catch((e) => res.status(400).send());
 });
 
+app.delete('/todos/:id', (req,res) =>{
+
+    var id = req.params.id;
+    console.log(id);
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findByIdAndRemove(id).then((todo) =>{
+        if(!todo){
+            return  res.status(404).send();
+        }
+        console.log(todo);
+        res.send(todo);
+    })
+    .catch((e) =>{
+        console.log(e);
+        res.status(400).send();
+    })
+})
+
 app.listen(port, () => {
     console.log(`Started server port - ${port}`);
 });
